f2271522f58e22655d99dcb0dbdddb525b3f3430
Sujit Sarkar <sweezit92@gmail.com>
2018-09-20 14:35:01 +0530

    push

---
+++ create_resume_step1.php
@@ -0,0 +1,260 @@
+
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<head>
+     
+
+    <meta name="google-site-verification" content="k4xe9OhSgLDZfAVXiuPA6wC5UDTbC09Ee2x6eWt3RFc" />
+
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="https://www.cvowl.com/static/homepage/assets/common/img/favicon.png">
+    
+    <meta name="description" content='How to Write Resume in 2018. How to create the perfect resume. Write your professional title and resume summary. Use resume keywords and action verbs. Resume Writing Tips.'>
+    
+    <meta name="keywords" content="Resume Builder, CV Maker, Free Resume Maker, Resume Templates | CV Owl">
+    
+    <meta property="og:url" content="https://www.cvowl.com/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="CV Owl" />
+    <meta property="og:description" content="World's Best and Free Online CV Builder - CV Owl" />
+    <meta property="og:image" content="/static/classic/topbar/assets/images/favicon.png"/>
+    
+    <meta charset="UTF-8">
+    <!-- Site Title -->
+    <title>How to Write Resume in 2018 | Best Resume Writing Tips | CV Owl</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,700,900" rel="stylesheet">
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+    <!-- ===========================================
+        CSS
+    ============================================= -->
+  <?php
+	include("metalinks.php");
+	?>
+  
+    
+</head>
+
+<style type="text/css">
+    
+    .version2 a.header-link{
+        color: black !important; 
+        cursor: pointer !important;
+    }
+
+    #mobile-nav .header-link{
+        color: white !important; 
+        cursor: pointer !important;
+        text-align: left;
+    }
+
+
+    .blog-post{
+        color: #212121;
+    }
+
+    .blog-heading{
+        font-size: 34px;
+        line-height: 50px;
+        color: white !important;
+        text-decoration: capitalise;
+    }
+    
+
+    @media (max-width: 768px) {
+        .onlymobile {
+            display: none;
+        }
+    }
+
+</style>
+
+
+<body class="version-blog">
+    <!-- start Header Area -->
+    <?php
+include("header.php");
+?>
+    <!-- End Header Area -->
+    <!-- Start hero-section -->
+<section class="hero-section-blog-details">
+    <div style="background: url('http://diversityhrsolutions.com/wp-content/uploads/2016/05/Great-Cover-Letter-Banner.jpg');">
+        <div class="text-center" style="margin:40px;">
+            <div class="col-md-12 pt-100">
+               <a class="blog-heading ">How To Create Resume</a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End hero-section -->
+
+<!-- Start main body contents -->
+<section class="blog-post ">
+    <div class="details-body">
+        <div class="container">
+            <div class="row">
+                <!-- Start Blog Details -->
+				<?php
+				include("resume_sidebar.php");
+				?>
+
+                <div class="col-lg-8" style="font-size: 16px;">
+                    <h2 class="head">Personal Information</h2>
+                    <form>
+					  <div class="form-row">
+						<div class="form-group col-md-4">
+						  <label for="inputEmail4">First Name</label>
+						  <input type="text" class="form-control" id="inputEmail4" placeholder="First Name">
+						</div>
+						<div class="form-group col-md-4">
+						  <label for="inputEmail4">Middle Name</label>
+						  <input type="text" class="form-control" id="inputEmail4" placeholder="Middle Name">
+						</div>
+						<div class="form-group col-md-4">
+						  <label for="inputPassword4">Last Name</label>
+						  <input type="text" class="form-control" id="inputPassword4" placeholder="Last Name">
+						</div>
+					  </div>
+					  <div class="form-row">
+						<div class="form-group col-md-6">
+						  <label for="inputEmail4">Professional Title/ Rank</label>
+						  <input type="text" class="form-control" id="inputEmail4" placeholder="First Name">
+						</div>
+						<div class="form-group col-md-6">
+						  <label for="inputEmail4">About Yourself</label>
+						  <input type="text" class="form-control" id="inputEmail4" placeholder="Middle Name">
+						</div>
+					  </div>
+					  <div class="form-row">
+						<div class='form-group date col-md-6' id='datepicker1'>
+							<label for="inputPassword4">Date Of Birth</label>
+							<input class="form-control" id="date" name="date" placeholder="MM/DD/YYY" type="text"/>
+						</div>
+						<div class="form-group col-md-6">
+						  <label for="inputPassword4">Nationality</label>
+						  <input type="text" class="form-control" id="inputPassword4" placeholder="Password">
+						</div>
+					  </div>
+					  <div class="form-row">
+						<div class="form-group col-md-6">
+						  <label for="inputCity">City</label>
+						  <input type="text" class="form-control" id="inputCity">
+						</div>
+						<div class="form-group col-md-4">
+						  <label for="inputState">State</label>
+						  <select id="inputState" class="form-control">
+							<option selected>Choose...</option>
+							<option>...</option>
+						  </select>
+						</div>
+						<div class="form-group col-md-2">
+						  <label for="inputZip">Zip</label>
+						  <input type="text" class="form-control" id="inputZip">
+						</div>
+					  </div>
+					  <div class="form-group">
+						<label for="inputAddress">Curren Address</label>
+						<input type="text" class="form-control" id="inputAddress" placeholder="Curren Address">
+					  </div>
+					  <div class="form-group">
+						<div class="form-check">
+						  <input class="form-check-input" type="checkbox" id="gridCheck">
+						  <label class="form-check-label" for="gridCheck">
+							Same as current address
+						  </label>
+						</div>
+					  </div>
+					  <div class="form-group">
+						<label for="inputAddress2">Permanent Address</label>
+						<input type="text" class="form-control" id="inputAddress2" placeholder="Permanent Address">
+					  </div>
+					  <div class="form-row">
+						<div class="form-group col-md-6">
+						  <label for="inputEmail4">Email</label>
+						  <input type="text" class="form-control" id="inputEmail4" placeholder="Email">
+						</div>
+						<div class="form-group col-md-6">
+						  <label for="inputEmail4">Phone</label>
+						  <input type="text" class="form-control" id="inputEmail4" placeholder="Phone">
+						</div>
+					  </div>
+					  <div class="form-row">
+						<div class="form-group col-md-6">
+						  <label for="inputEmail4">Visa</label>
+						  <select id="inputState" class="form-control">
+							<option selected>Choose...</option>
+							<option value="1">Yes</option>
+							<option value="2">No</option>
+						  </select>
+						</div>
+						<div class="form-group col-md-6">
+						  <label for="inputEmail4">Visa Category</label>
+						  <select id="inputState" class="form-control">
+							<option selected>Choose...</option>
+							<option value="1">B1</option>
+							<option value="2">B2</option>
+						  </select>
+						</div>
+					  </div>
+					  <div class="form-row">
+						<div class="form-group col-md-6">
+						  <label for="inputEmail4">Seaman's Book</label>
+						  <select id="inputState" class="form-control">
+							<option selected>Choose...</option>
+							<option value="1">Yes</option>
+							<option value="2">No</option>
+						  </select>
+						</div>
+						<div class="form-group col-md-6">
+						  <label for="inputEmail4">Smoke</label>
+						  <select id="inputState" class="form-control">
+							<option selected>Choose...</option>
+							<option value="1">Yes</option>
+							<option value="2">No</option>
+						  </select>
+						</div>
+					  </div>
+					  <div class="form-row">
+						<div class="form-group col-md-6">
+						  <label for="inputEmail4">When you can start the new job?</label>
+						  <select id="inputState" class="form-control">
+							<option selected>Choose...</option>
+							<option value="1">Now</option>
+							<option value="2">In 1 Month</option>
+							<option value="2">In 2 Month</option>
+						  </select>
+						</div>
+						<div class="form-group col-md-6">
+						  <label for="inputEmail4">Visible Tattos</label>
+						  <select id="inputState" class="form-control">
+							<option selected>Choose...</option>
+							<option value="1">Yes</option>
+							<option value="2">No</option>
+						  </select>
+						</div>
+					  </div>
+					  
+					  
+					  <button type="submit" class="btn btn-primary">Next</button>
+					</form>
+                    <hr>
+                </div>
+              
+				
+
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+include("footer.php");
+?>
+    
+</body>
+
+</html>
---
+++ resume_sidebar.php
@@ -0,0 +1,24 @@
+<div class="col-lg-4">
+					<div class="side-bar">
+						<div class="post-cat">
+							<div class="cat-box">
+								<a class="cat-link" href="">Personal Information</a>
+							</div>
+							<div class="cat-box">
+								<a class="cat-link" href="">Education</a>
+							</div>
+							<div class="cat-box">
+								<a class="cat-link" href="">Business Analyst Resume</a>
+							</div>
+							<div class="cat-box">
+								<a class="cat-link" href="">Sales Executive Resume</a>
+							</div>
+							<div class="cat-box">
+								<a class="cat-link" href="">Accountant Resume</a>
+							</div>
+							<div class="cat-box">
+								<a class="cat-link" href="">HR Executive Resume</a>
+							</div>
+						</div>
+					</div>
+				</div>
